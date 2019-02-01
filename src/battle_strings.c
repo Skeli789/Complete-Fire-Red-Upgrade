01a8b5e094ed49b8acd9e4be31ac57db06485d8e
Skeli <sesyl.schweitzer@gmail.com>
2019-02-01 14:02:41 -0500

    Added link support for custom strings
    Set over the contents of the BattleStringLoader to link opponents so they can print custom strings too.

--- src/battle_strings.c
+++ src/battle_strings.c
@@ -831,17 +831,17 @@ u32 BattleStringExpandPlaceholders(const u8* src, u8* dst)
 
 void EmitPrintStringUpdate(void)
 {
-    sBattleBuffersTransferData[0] = CONTROLLER_PRINTSTRING;
-    sBattleBuffersTransferData[1] = gBattleStringLoader;
-    sBattleBuffersTransferData[2] = gBattleStringLoader >> 0x8;
-    sBattleBuffersTransferData[3] = gBattleStringLoader >> 0x10;
-	sBattleBuffersTransferData[4] = gBattleStringLoader >> 0x18;
-    PrepareBufferDataTransfer(1, sBattleBuffersTransferData, 5);
+    gBattleBuffersTransferData[0] = CONTROLLER_PRINTSTRING;
+    gBattleBuffersTransferData[1] = (u32) BattleStringLoader;
+    gBattleBuffersTransferData[2] = ((u32) BattleStringLoader) >> 0x8;
+    gBattleBuffersTransferData[3] = ((u32) BattleStringLoader) >> 0x10;
+	gBattleBuffersTransferData[4] = ((u32) BattleStringLoader) >> 0x18;
+    PrepareBufferDataTransfer(1, gBattleBuffersTransferData, 5);
 }
 
 void PlayerHandlePrintStringUpdate(void)
 {
-	BattleStringLoader = T1_READ_PTR(gBattleBufferB[gActiveBattler][1]);
+	BattleStringLoader = T1_READ_PTR(&gBattleBufferB[gActiveBattler][1]);
     gBattle_BG0_X = 0;
     gBattle_BG0_Y = 0;
 }
