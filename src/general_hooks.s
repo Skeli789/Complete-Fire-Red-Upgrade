15072b29e280294c74d1064f962109cb032842d9
Skeli <sesyl.schweitzer@gmail.com>
2019-02-10 21:13:59 -0500

    Update general_hooks.s

--- src/general_hooks.s
+++ src/general_hooks.s
@@ -71,3 +71,33 @@ SlideMonToOriginalPosHook:
 	ldr r1, =0x2023D44 @BattlerSpriteIds
 	ldr r2, =0x80991DE | 1
 	bx r2
+
+@0x0804AA1E with r1
+GetProperBallIdHook1:
+	bl BallIdToItemId
+	bl ItemIdToBallId
+	lsl r0, #0x18
+	lsr r0, #0x18
+	mov r5, r0
+	ldr r1, =0x804AA28 | 1
+	bx r1
+
+@0x080EF3A8 with r1
+GetProperBallIdHook2:
+	bl BallIdToItemId
+	bl ItemIdToBallId
+	lsl r0, #0x18
+	lsr r0, #0x18
+	ldr r1, =0x80EF3B0 | 1
+	bx r1
+
+@Summary Screen Poke Ball Hook
+@0x08139CDA with r1
+GetProperBallIdHook3:
+	bl BallIdToItemId
+	bl ItemIdToBallId
+	mov r4, r0
+	lsl r4, #0x18
+	lsr r4, #0x18
+	ldr r1, =0x8139CE4 | 1
+	bx r1
