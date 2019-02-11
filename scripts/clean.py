a28f99069a610bc7a92925991d57b4bc84a70d0b
Skeli <sesyl.schweitzer@gmail.com>
2019-02-10 21:14:20 -0500

    Now removes generated .h files.

--- scripts/clean.py
+++ scripts/clean.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import glob, os.path
 
 ROM_NAME = "test.gba"
 
@@ -24,5 +25,11 @@ try:
 	shutil.rmtree('build/')
 except:
 	pass
+	
+os.chdir("src\\Images")
+for root, dirs, files in os.walk(".", topdown = False):
+	for file in files:
+		if file.endswith('.h'):
+			os.remove(os.path.join(root, file))
 
 print("Directory cleaned!")
