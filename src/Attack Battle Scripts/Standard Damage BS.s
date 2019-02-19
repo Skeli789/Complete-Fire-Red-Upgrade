4927829f5db88cc28380386be240d94ce3d7b5b8
Skeli <sesyl.schweitzer@gmail.com>
2019-02-18 19:55:43 -0500

    Update Standard Damage BS.s

--- src/Attack Battle Scripts/Standard Damage BS.s
+++ src/Attack Battle Scripts/Standard Damage BS.s
@@ -6,18 +6,29 @@
 
 .global STANDARD_DAMAGE
 STANDARD_DAMAGE:
-attackstring
-ppreduce
-calculatedamage
-attackanimation
-waitanimation
-missmessage
-flash BANK_TARGET
-waitstateatk
-graphicalhpupdate BANK_TARGET
-datahpupdate BANK_TARGET
-critmessage
-waitmessage DELAY_1SECOND
-resultmessage
-waitmessage DELAY_1SECOND
-return
+	attackstring
+	ppreduce
+	calculatedamage
+	attackanimation
+	waitanimation
+	effectivenesssound
+	flash BANK_TARGET
+	waitstateatk
+	graphicalhpupdate BANK_TARGET
+	datahpupdate BANK_TARGET
+	critmessage
+	waitmessage DELAY_1SECOND
+	resultmessage
+	waitmessage DELAY_1SECOND
+	return
+
+.global FAILED
+FailedPreBS:
+	attackstring
+	ppreduce
+FAILED:
+	pause DELAY_HALFSECOND
+	orbyte OUTCOME OUTCOME_FAILED
+	resultmessage
+	waitmessage DELAY_1SECOND
+	goto BS_MOVE_END
