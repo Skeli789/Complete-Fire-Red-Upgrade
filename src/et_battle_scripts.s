9da49ef146b26056aa7891a752e42f35e647d72f
Skeli <sesyl.schweitzer@gmail.com>
2019-02-18 19:53:42 -0500

    Added future sight damage script

--- src/et_battle_scripts.s
+++ src/et_battle_scripts.s
@@ -43,6 +43,8 @@
 .global BattleScript_ShieldsDownToCore
 .global BattleScript_ShieldsDownToMeteor
 .global BattleScript_FlowerGift
+.global BattleScript_MonTookFutureAttack
+
 .global BattleScript_Victory @More of an "End Battle" BS but whatever
 .global BattleScript_PrintPlayerForfeited
 .global BattleScript_PrintPlayerForfeitedLinkBattle
@@ -50,6 +52,8 @@
 .global BattleScript_LostBattleTower
 
 .global AbilityActivatedString
+
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 @0x80804AC with r0
 EndBattleFlagClearHook: @Not really a BS but whatever
@@ -71,6 +75,8 @@ NotOakBattle:
 .EndBattleOakType: .word 0x80804B6 | 1
 .EndBattleRegular: .word 0x8080508 | 1
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 BattleScript_RainDishActivates:
 	playanimation BANK_ATTACKER ANIM_HEALING_SPARKLES 0x0
 	orword HIT_MARKER 0x100
@@ -79,6 +85,8 @@ BattleScript_RainDishActivates:
 	printstring 0x133
 	waitmessage DELAY_1SECOND
 	end3
+
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_DrySkinDamage:
 BattleScript_SolarPowerDamage:
@@ -90,6 +98,8 @@ BattleScript_SolarPowerDamage:
 	waitmessage DELAY_1SECOND
 	faintpokemon BANK_ATTACKER 0x0 0x0
 	end2
+
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_SeaOfFireDamage:
 	callasm SeaOfFireDamageASM+1
@@ -103,6 +113,8 @@ BattleScript_SeaOfFireDamage:
 	faintpokemon BANK_ATTACKER 0x0 0x0
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 BattleScript_GrassyTerrainHeal:
 	callasm GrassyTerrainHeal+1
 	playanimation BANK_ATTACKER ANIM_GRASSY_TERRAIN_HEAL 0x0
@@ -114,6 +126,7 @@ BattleScript_GrassyTerrainHeal:
 	waitmessage DELAY_1SECOND
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_Healer:
 	setword BATTLE_STRING_LOADER, HealerString
@@ -123,6 +136,7 @@ BattleScript_Healer:
 HealerBSEnd:
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_AquaRing:
 	playanimation BANK_ATTACKER ANIM_AQUA_RING_HEAL 0x0
@@ -134,6 +148,7 @@ BattleScript_AquaRing:
 	waitmessage DELAY_1SECOND
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_PoisonHeal:
 	playanimation BANK_ATTACKER ANIM_HEALING_SPARKLES 0x0
@@ -145,6 +160,7 @@ BattleScript_PoisonHeal:
 	waitmessage DELAY_1SECOND
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_MagnetRiseEnd:
 	clearspecialstatusbit BANK_ATTACKER STATUS3_LEVITATING
@@ -153,6 +169,7 @@ BattleScript_MagnetRiseEnd:
 	waitmessage DELAY_1SECOND
     end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_TelekinesisEnd:
 	clearspecialstatusbit BANK_ATTACKER STATUS3_TELEKINESIS
@@ -161,6 +178,7 @@ BattleScript_TelekinesisEnd:
 	waitmessage DELAY_1SECOND
     end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_HealBlockEnd:
 	setword BATTLE_STRING_LOADER HealBlockEndString
@@ -168,6 +186,7 @@ BattleScript_HealBlockEnd:
 	waitmessage DELAY_1SECOND
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_EmbargoEnd:
 	setword BATTLE_STRING_LOADER EmbargoEndString
@@ -175,6 +194,7 @@ BattleScript_EmbargoEnd:
 	waitmessage DELAY_1SECOND
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_AuroraVeilEnd:
 	jumpifbyte ANDS USER_BANK 0x1 FoeAuroraVeil
@@ -187,6 +207,7 @@ FoeAuroraVeil:
 	setword BATTLE_STRING_LOADER FoeAuroraVeilEndString
 	goto PrintTimerString
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_TailwindEnd:
 	jumpifbyte ANDS USER_BANK 0x1 FoeTailwind
@@ -199,6 +220,7 @@ FoeTailwind:
 	setword BATTLE_STRING_LOADER FoeTailwindEndString
 	goto PrintTimerString
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_LuckyChantEnd:
 	jumpifbyte ANDS USER_BANK 0x1 FoeLuckyChant
@@ -211,6 +233,7 @@ FoeLuckyChant:
 	setword BATTLE_STRING_LOADER FoeLuckyChantEndString
 	goto PrintTimerString
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_RainbowEnd:
 	jumpifbyte ANDS USER_BANK 0x1 FoeRainbow
@@ -223,6 +246,7 @@ FoeRainbow:
 	setword BATTLE_STRING_LOADER FoeRainbowEndString
 	goto PrintTimerString
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_SeaOfFireEnd:
 	jumpifbyte ANDS USER_BANK 0x1 FoeSeaOfFire
@@ -235,6 +259,7 @@ FoeSeaOfFire:
 	setword BATTLE_STRING_LOADER FoeSeaOfFireEndString
 	goto PrintTimerString
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_SwampEnd:
 	jumpifbyte ANDS USER_BANK 0x1 FoeSwamp
@@ -247,6 +272,7 @@ FoeSwamp:
 	setword BATTLE_STRING_LOADER FoeSwampEndString
 	goto PrintTimerString
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_TrickRoomEnd:
 	setword BATTLE_STRING_LOADER TrickRoomEndString
@@ -282,11 +308,14 @@ BattleScript_TerrainEnd:
 	setbyte TERRAIN_BYTE 0x0
 	setword BATTLE_STRING_LOADER TerrainEndString
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 PrintTimerString:
 	printstring 0x184
 	waitmessage DELAY_1SECOND
 	end2
 	
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_MoodySingleStat:
 	setword BATTLE_STRING_LOADER AbilityActivatedString
@@ -316,6 +345,7 @@ MoodyRegularP2:
 	setbyte MOODY_STAT_LOWER_BYTE 0x0
 	goto MoodySkipString
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_BadDreams:
 	setbyte SEED_HELPER 0x0
@@ -371,6 +401,7 @@ BadDreams_end:
 	setbyte SEED_HELPER 0x0
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_ToxicOrb:
 	setbyte POISONED_BY 0x3
@@ -379,6 +410,7 @@ BattleScript_ToxicOrb:
 	seteffecttarget
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_FlameOrb:
 	setbyte POISONED_BY 0x3
@@ -387,6 +419,7 @@ BattleScript_FlameOrb:
 	seteffecttarget
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_Harvest:
 	recycleitem HarvestBSEnd
@@ -399,6 +432,7 @@ HarvestBSEnd:
 	setbyte FORM_COUNTER 0x0
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_Pickup:
 	copyarray USER_BANK 0x2023BC4 0x1
@@ -407,6 +441,7 @@ BattleScript_Pickup:
 	waitmessage DELAY_1SECOND
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_ZenMode:
 	setword BATTLE_STRING_LOADER AbilityActivatedString
@@ -415,6 +450,7 @@ BattleScript_ZenMode:
 	playanimation BANK_ATTACKER ANIM_TRANSFORM 0x0
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_PowerConstruct:
 	setword BATTLE_STRING_LOADER PresenceOfManyString
@@ -427,6 +463,7 @@ BattleScript_PowerConstruct:
 	waitmessage DELAY_1SECOND
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_StartedSchooling:
 	playanimation BANK_ATTACKER ANIM_WISHIWASHI_FISH 0x0
@@ -442,6 +479,7 @@ BattleScript_StoppedSchooling:
 	waitmessage DELAY_1SECOND
 	end2
 
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_ShieldsDownToCore:
 	playanimation BANK_ATTACKER ANIM_TRANSFORM 0x0
@@ -450,13 +488,14 @@ BattleScript_ShieldsDownToCore:
 	waitmessage DELAY_1SECOND
 	end2
 
-
 BattleScript_ShieldsDownToMeteor:
 	playanimation BANK_ATTACKER ANIM_TRANSFORM 0x0
 	setword BATTLE_STRING_LOADER ToMeteorString
 	printstring 0x184
 	waitmessage DELAY_1SECOND
 	end2
+
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_FlowerGift:
 	playanimation BANK_ATTACKER ANIM_TRANSFORM 0x0
@@ -465,7 +504,57 @@ BattleScript_FlowerGift:
 	waitmessage DELAY_1SECOND
 	end2
 
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+BattleScript_MonTookFutureAttack:
+	printstring 0xA2 @;STRINGID_PKMNTOOKATTACK
+	waitmessage DELAY_1SECOND
+	jumpifmove MOVE_DOOMDESIRE BattleScript_CheckDoomDesireMiss
+	accuracycheck BattleScript_FutureAttackMiss MOVE_FUTURESIGHT
+	goto BattleScript_CalcDamage
+
+BattleScript_CheckDoomDesireMiss:
+	accuracycheck BattleScript_FutureAttackMiss MOVE_DOOMDESIRE
+
+BattleScript_CalcDamage:
+	critcalc
+	callasm FutureSightDamageCalc + 1
+	typecalc
+	adjustnormaldamage2
+	jumpifmove MOVE_DOOMDESIRE BattleScript_FutureHitAnimDoomDesire
+	playanimation BANK_ATTACKER ANIM_FUTURE_SIGHT_HIT 0x0
+	goto BattleScript_DoFutureAttackHit
+
+BattleScript_FutureHitAnimDoomDesire:
+	playanimation BANK_ATTACKER ANIM_DOOM_DESIRE_HIT 0x0
+
+BattleScript_DoFutureAttackHit:
+	effectivenesssound
+	flash BANK_TARGET
+	waitstateatk
+	graphicalhpupdate BANK_TARGET
+	datahpupdate BANK_TARGET
+	resultmessage
+	waitmessage DELAY_1SECOND
+	faintpokemon BANK_TARGET 0x0 0x0
+	ifwildbattleend BattleScript_FutureAttackEnd
+
+BattleScript_FutureAttackEnd:
+	setbyte CMD49_STATE 0x0
+	cmd49 0x3 0x0
+	setbyte OUTCOME 0
+	end2
+	
+BattleScript_FutureAttackMiss:
+	pause DELAY_HALFSECOND
+	setbyte OUTCOME 0
+	orbyte OUTCOME OUTCOME_FAILED
+	resultmessage
+	waitmessage DELAY_1SECOND
+	setbyte OUTCOME 0
+	end2
+
+@;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 BattleScript_Victory:
 	jumpifword ANDS BATTLE_TYPE BATTLE_TWO_OPPONENTS BeatTwoPeeps
