a41ffac78c352d34434e1de142074a938d7b26ae
Skeli <sesyl.schweitzer@gmail.com>
2019-03-22 17:55:45 -0400

    Fix RTC Comment Error

--- src/Assembly Routines/rtc.s
+++ src/Assembly Routines/rtc.s
@@ -16,7 +16,7 @@
 0300553E day of week (1 byte) 
 0300553F month (1 byte)
 03005540 day (1 byte)
-03005541 day of week (1 byte)
+03005541 0x0 (1 byte)
 03005542 hour (24-hr format, 1 byte)
 03005543 min (1 byte)
 03005544 second (1 byte)
