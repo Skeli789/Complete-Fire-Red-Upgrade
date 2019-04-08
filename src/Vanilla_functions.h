73390b7a61ecae88f0b8814196fd80622e8f7760
Skeli <sesyl.schweitzer@gmail.com>
2019-04-08 14:57:15 -0400

    PokeIcon Functions

--- src/Vanilla_functions.h
+++ src/Vanilla_functions.h
@@ -259,6 +259,21 @@ bool8 __attribute__((long_call)) GetIndexFromDexFlag(u16 index, u8 dexFlag);
 
 u8 __attribute__((long_call)) GetWildDataIndexForMap(void);
 
+//Pokemon Icon Functions
+u8 CreateMonIcon(u16 species, void (*callback)(struct Sprite *), s16 x, s16 y, u8 subpriority, u32 personality, bool32 extra);
+const u8* __attribute__((long_call)) GetMonIconTiles(u16 species, bool32 extra);
+void __attribute__((long_call)) LoadMonIconPalettes(void);
+void __attribute__((long_call)) SafeLoadMonIconPalette(u16 species);
+void __attribute__((long_call)) LoadMonIconPalette(u16 species);
+void __attribute__((long_call)) FreeMonIconPalettes(void);
+void __attribute__((long_call)) SafeFreeMonIconPalette(u16 species);
+void __attribute__((long_call)) FreeMonIconPalette(u16 species);
+void __attribute__((long_call)) SpriteCB_PokeIcon(struct Sprite *sprite);
+const u16* __attribute__((long_call)) GetValidMonIconPalettePtr(u16 species);
+u8 __attribute__((long_call)) GetValidMonIconPalIndex(u16 species);
+u8 __attribute__((long_call)) GetSpeciesIconPaletteIndex(u16 species);
+const u16* __attribute__((long_call)) GetValidMonIconPalettePtr(u16 species);
+
 //Wild Encounter Functions
 u8 __attribute__((long_call)) ChooseWildMonIndex_Land(void);
 u8 __attribute__((long_call)) ChooseWildMonIndex_WaterRock(void);
