df3f20184c53d8cc2d3f2bb0086814bea10f7a71
Skeli <sesyl.schweitzer@gmail.com>
2020-07-15 21:51:58 +0300

    Fix Special 0x40 Layer Tiles

--- assembly/triple_layer_blocks.s
+++ assembly/triple_layer_blocks.s
@@ -16,17 +16,17 @@ triple_layer_blocks.s
 DrawBlockHook:
 	lsl r2, r2, #0x10
 	lsr r2, r2, #0x10
-	cmp r0, #0x3
+	cmp r0, #0x4
 	bhi SkipTripleLayer
 	lsl r0, r0, #0x2
 	ldr r3, =sOverworldBlockFuncs
 	add r0, r0, r3
 	ldr r0, [r0]
 	mov pc, r0
-	
+
 SkipTripleLayer:
-	ldr r0, =(0x0805AABE +1)
-	bx r0
+	ldr r1, =(0x0805AABE +1)
+	bx r1
 		
 .align 2
 .pool
@@ -80,10 +80,14 @@ HandleTripleBlock:
 
 .align 2
 sOverworldBlockFuncs:
-.word 0x805aa6c, 0x805a9ce, 0x805aa0c, HandleTripleBlock
+.word 0x805aa6c @LAYER_TYPE_NORMAL
+.word 0x805aa6c @LAYER_TYPE_NORMAL
+.word 0x805aa0c @LAYER_TYPE_COVERED_BY_OBJECTS
+.word HandleTripleBlock
+.word 0x805a9ce @LAYER_TYPE_BG3_BG1
 
 .align 2
 .address:	.word 0x0300501C
 .address1:	.word 0x03005014
 .address2:	.word 0x03005018
-.return:		.word 0x0805AABF
+.return:	.word 0x0805AABF
