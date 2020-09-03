7b96209819d8b0bebeaec31a50acce0c7ec6a0a3
Skeli <sesyl.schweitzer@gmail.com>
2020-09-03 09:23:08 +0300

    Fix Cursed Body Disabling Wrong Move

--- src/general_bs_commands.c
+++ src/general_bs_commands.c
@@ -3247,22 +3247,24 @@ void atkA2_mirrorcoatdamagecalculator(void) {
 
 void atkA3_disablelastusedattack(void)
 {
-	int i;
-	u8 bankDef;
+	u8 i, bankDef;
+	u16 lastMove;
 
 	if (gBattlescriptCurrInstr[1] == BS_GET_TARGET)
-		bankDef = gBankTarget;
-	else
-		bankDef = gBankAttacker; //For Cursed Body
-
-	for (i = 0; i < MAX_MON_MOVES; i++)
 	{
-		if (gBattleMons[bankDef].moves[i] == gLastUsedMoves[bankDef])
-			break;
+		bankDef = gBankTarget;
+		lastMove = gLastUsedMoves[bankDef];
+	}
+	else
+	{
+		bankDef = gBankAttacker; //For Cursed Body
+		lastMove = gCurrentMove;
 	}
 
-	if (gDisableStructs[bankDef].disabledMove == 0
-	&& i != MAX_MON_MOVES
+	i = FindMovePositionInMoveset(lastMove, bankDef);
+
+	if (i < MAX_MON_MOVES
+	&& gDisableStructs[bankDef].disabledMove == 0
 	&& gBattleMons[bankDef].pp[i] != 0
 	&& !IsAnyMaxMove(gLastUsedMoves[bankDef])
 	&& !IsZMove(gLastUsedMoves[bankDef])
