abb7abfbda83fdbdb1cbaf1b068d9d8bdbdd5777
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2020-11-02 11:04:33 -0500

    Add instant text option.

--- include/new_menu_helpers.h
+++ include/new_menu_helpers.h
@@ -14,5 +14,6 @@ u8 __attribute((long_call)) GetPlayerTextSpeed(void);
 void __attribute__((long_call)) DrawDialogueFrameNewMenuHelpers(u8 windowId, bool8 transfer);
 u16 __attribute__((long_call)) AddTextPrinterParameterized2(u8 windowId, u8 fontId, const u8 *str, u8 speed, void (*callback)(struct TextPrinterTemplate *, u16), u8 fgColor, u8 bgColor, u8 shadowColor);
 void __attribute__((long_call)) DrawHelpMessageWindowWithText(const u8 * text);
+u8 __attribute__((long_call)) GetTextSpeedSetting(void);
 
 #endif // GUARD_NEW_MENU_HELPERS_H
