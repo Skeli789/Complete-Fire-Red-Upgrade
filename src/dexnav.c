df857e79b49d61d4b1cb8ba46ac7d6cb1f45e045
Cincidial <22229883+Cincidial@users.noreply.github.com>
2020-12-09 18:54:57 -0700

    Fixed DexNav Pokemon IVs not matching potential

--- src/dexnav.c
+++ src/dexnav.c
@@ -179,19 +179,22 @@ void DexNavGetMon(u16 species, u8 potential, u8 level, u8 ability, u16* moves, u
 
 	//Pick potential ivs to set to 31
 	u8 iv[3];
-	for (i = 0; i < NELEMS(iv); ++i)
+	for (i = 0; i < NELEMS(iv); ++i) 
 		iv[i] = Random() % 6;
 
-	if ((iv[0] != iv[1]) && (iv[0] != iv[2]) && (iv[1] != iv[2]))
-	{
-		u8 perfectIv = 0x1F;
-		if (potential > 2)
-			SetMonData(mon, MON_DATA_HP_IV + iv[2], &perfectIv);
-		else if (potential > 1)
-			SetMonData(mon, MON_DATA_HP_IV + iv[1], &perfectIv);
-		else if (potential)
-			SetMonData(mon, MON_DATA_HP_IV + iv[0], &perfectIv);
-	}
+	while (iv[1] == iv[0]) 
+		iv[1] = Random() % 6;
+
+	while (iv[2] == iv[0] || iv[2] == iv[1]) 
+		iv[2] = Random() % 6;
+
+	u8 perfectIv = 0x1F;
+	if (potential > 2)
+		SetMonData(mon, MON_DATA_HP_IV + iv[2], &perfectIv);
+	if (potential > 1)
+		SetMonData(mon, MON_DATA_HP_IV + iv[1], &perfectIv);
+	if (potential)
+		SetMonData(mon, MON_DATA_HP_IV + iv[0], &perfectIv);
 
 	//Set ability
 	if (gBaseStats[species].hiddenAbility == ability)
