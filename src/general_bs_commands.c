69fa06962cf043d01bced6af8ac4095ae41d6a32
Greenphx9 <55181320+Greenphx9@users.noreply.github.com>
2022-07-05 17:14:42 +1000

    Cursed Body Fix

--- src/general_bs_commands.c
+++ src/general_bs_commands.c
@@ -3241,10 +3241,10 @@ void atkA3_disablelastusedattack(void)
 	int i;
 	u8 bankDef;
 
-	if (gBattlescriptCurrInstr[1] == BS_GET_TARGET)
-		bankDef = gBankTarget;
-	else
+	if(gLastUsedAbility == ABILITY_CURSEDBODY)
 		bankDef = gBankAttacker; //For Cursed Body
+	else
+		bankDef = gBankTarget; //For Disable
 
 	for (i = 0; i < MAX_MON_MOVES; i++)
 	{
