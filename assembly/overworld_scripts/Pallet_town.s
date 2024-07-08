a4ea8ba9dd732af0eea60287283f3ceabe4a1a52
Shiny Master of all <101048219+Shiny-Miner@users.noreply.github.com>
2024-07-08 17:00:36 +0530

    Add files via upload

---
+++ assembly/overworld_scripts/Pallet_town.s
@@ -0,0 +1,11 @@
+.thumb
+.align 2
+
+.include "../xse_commands.s"
+.include "../xse_defines.s"
+
+.global EventScript_PalletTown_FatGuy
+EventScript_PalletTown_FatGuy:
+    setflag 0xa03
+    msgbox gText_PalletTown_FatGuy MSG_FACE
+    end
