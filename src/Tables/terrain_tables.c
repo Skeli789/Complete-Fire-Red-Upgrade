62b46f40b4c79ec5684e7911008ed825349bfe59
Zake <zakeplaybr@gmail.com>
2024-07-23 11:39:39 -0300

    Added new secret power terrain tables (9th commit)

--- src/Tables/terrain_tables.c
+++ src/Tables/terrain_tables.c
@@ -165,12 +165,12 @@ const struct TerrainTableStruct gTerrainTable[] =
 			.burmyForm = SPECIES_BURMY_TRASH,
 		},
 
-	[BATTLE_TERRAIN_SPOOKY + 4] =
+	[BATTLE_TERRAIN_INSIDE_3 + 4] =
 		{
-			.camouflageType = TYPE_GHOST,
-			.secretPowerEffect = MOVE_EFFECT_EVS_MINUS_1,
-			.secretPowerAnim = MOVE_SHADOWSNEAK,
-			.naturePowerMove = MOVE_SHADOWBALL,
+			.camouflageType = TYPE_NORMAL,
+			.secretPowerEffect = MOVE_EFFECT_PARALYSIS,
+			.secretPowerAnim = MOVE_BODYSLAM,
+			.naturePowerMove = MOVE_TRIATTACK,
 			.burmyForm = SPECIES_BURMY_TRASH,
 		},
 
@@ -183,13 +183,13 @@ const struct TerrainTableStruct gTerrainTable[] =
 			.burmyForm = SPECIES_BURMY_TRASH,
 		},
 
-	[BATTLE_TERRAIN_DESERT + 4] =
+	[BATTLE_TERRAIN_INSIDE_5 + 4] =
 		{
-			.camouflageType = TYPE_GROUND,
-			.secretPowerEffect = MOVE_EFFECT_SPD_MINUS_1,
-			.secretPowerAnim = MOVE_MUDSHOT,
-			.naturePowerMove = MOVE_MUDBOMB,
-			.burmyForm = SPECIES_BURMY_SANDY,
+			.camouflageType = TYPE_NORMAL,
+			.secretPowerEffect = MOVE_EFFECT_PARALYSIS,
+			.secretPowerAnim = MOVE_BODYSLAM,
+			.naturePowerMove = MOVE_TRIATTACK,
+			.burmyForm = SPECIES_BURMY_TRASH,
 		},
 
 	[BATTLE_TERRAIN_INSIDE_6 + 4] =
@@ -249,6 +249,78 @@ const struct TerrainTableStruct gTerrainTable[] =
 #endif
 	//ADD NEW ENTRIES HERE
 
+	[BATTLE_TERRAIN_SPOOKY + 4] =
+		{
+			.camouflageType = TYPE_GHOST,
+			.secretPowerEffect = MOVE_EFFECT_EVS_MINUS_1,
+			.secretPowerAnim = MOVE_SHADOWSNEAK,
+			.naturePowerMove = MOVE_SHADOWBALL,
+			.burmyForm = SPECIES_BURMY_TRASH,
+		},
+
+	[BATTLE_TERRAIN_DESERT + 4] =
+		{
+			.camouflageType = TYPE_GROUND,
+			.secretPowerEffect = MOVE_EFFECT_SPD_MINUS_1,
+			.secretPowerAnim = MOVE_MUDSHOT,
+			.naturePowerMove = MOVE_MUDBOMB,
+			.burmyForm = SPECIES_BURMY_SANDY,
+		},
+
+	[BATTLE_TERRAIN_TORMA + 4] =
+		{
+			.camouflageType = TYPE_ROCK,
+			.secretPowerEffect = MOVE_EFFECT_FLINCH,
+			.secretPowerAnim = MOVE_ROCKTHROW,
+			.naturePowerMove = MOVE_POWERGEM,
+			.burmyForm = SPECIES_BURMY_SANDY,
+		},
+
+	[BATTLE_TERRAIN_TORMA_DEPTHS + 4] =
+		{
+			.camouflageType = TYPE_ICE,
+			.secretPowerEffect = MOVE_EFFECT_FREEZE,
+			.secretPowerAnim = MOVE_AVALANCHE,
+			.naturePowerMove = MOVE_FROSTBREATH,
+			.burmyForm = SPECIES_BURMY_SANDY,
+		},
+
+	[BATTLE_TERRAIN_SCALDING_SPA + 4] =
+		{
+			.camouflageType = TYPE_ICE,
+			.secretPowerEffect = MOVE_EFFECT_BURN,
+			.secretPowerAnim = MOVE_FLAMECHARGE,
+			.naturePowerMove = MOVE_FLAREBLITZ,
+			.burmyForm = SPECIES_BURMY_SANDY,
+		},
+
+	[BATTLE_TERRAIN_GYM + 4] =
+		{
+			.camouflageType = TYPE_NORMAL,
+			.secretPowerEffect = MOVE_EFFECT_PARALYSIS,
+			.secretPowerAnim = MOVE_BODYSLAM,
+			.naturePowerMove = MOVE_TRIATTACK,
+			.burmyForm = SPECIES_BURMY_TRASH,
+		},
+
+	[BATTLE_TERRAIN_FOREST + 4] =
+		{
+			.camouflageType = TYPE_NORMAL,
+			.secretPowerEffect = MOVE_EFFECT_PREVENT_ESCAPE,
+			.secretPowerAnim = MOVE_INGRAIN,
+			.naturePowerMove = MOVE_POWERWHIP,
+			.burmyForm = SPECIES_BURMY,
+		},
+
+	[BATTLE_TERRAIN_FOREST_PERADON + 4] =
+		{
+			.camouflageType = TYPE_NORMAL,
+			.secretPowerEffect = MOVE_EFFECT_PREVENT_ESCAPE,
+			.secretPowerAnim = MOVE_INGRAIN,
+			.naturePowerMove = MOVE_POWERWHIP,
+			.burmyForm = SPECIES_BURMY,
+		},
+		
 #ifdef UNBOUND //The ones below are for Pokemon Unbound. Feel free to remove.
 	[BATTLE_TERRAIN_MOLEMAN + 4] =
 		{
