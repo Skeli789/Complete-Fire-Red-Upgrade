7179b16560b3fed0b965a6fe0f59277d33412931
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2024-09-24 02:54:40 +0530

    Update config.h

--- src/config.h
+++ src/config.h
@@ -346,6 +346,7 @@ enum //These vars need to be one after the other (hence the enum)
 #define FROSTBITE //Replaces the Freeze status condition with the Frostbite status condition from PLA
 #define EXPANDED_NEW_ITEMS //Expands items table and add new items util gen 9.
 #define SHOWPOKEPIC_WITHOUT_UGLY_BOX //Fixes the ShowPokePic command to only show Pokemon sprite and not a box with it
+#define CHANGE_ITEM_STORAGE_GRAPHIC //if uncommented, will change pc item storage to Swsh style (background modificable)
 
 /* DexNav Options */
 //See "include/new/dexnav_config.h"
